2828a3bfaa58941570820593b5b7d115e50faf35
Karol Wasemann <kw.kwaq@gmail.com>
2022-02-24 09:26:36 +0100

    Task solved

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
-// Import stylesheets
-import './style.css';
+const modal = document.getElementById("modal");
+const btn = document.getElementById("btn");
+const monster = document.querySelector(".monster");
+
+const modalHide = () => {
+  if (localStorage.getItem("agreed")) {
+    modal.style.display = "none";
+    monster.style.display = "none";
+  }
+};
+const handleLocalStorage = (e) => {
+  e.preventDefault();
+  localStorage.setItem("agreed", "true");
+  modalHide();
+};
+
+modalHide();
+btn.addEventListener("click", handleLocalStorage);
